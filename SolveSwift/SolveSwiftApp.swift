6fd6ab8e1a2cb627c246cb3e09a26fda3b82d4a0
Evgeny <zzzzbh@gmail.com>
2023-03-24 12:42:49 +0000

    Initial Commit

---
+++ SolveSwift/SolveSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolveSwiftApp.swift
+//  SolveSwift
+//
+//  Created by Евгений Басистый on 24.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SolveSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
